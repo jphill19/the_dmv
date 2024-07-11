d91cc4228216cef11cc55cbe2a8d18994072b064
John Hill <jphill1989@gmail.com>
2024-07-11 16:46:21 -0600

    Created new attributes @registered_vehicles & @collected_fess, and tested

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,12 +1,13 @@
 class Facility
-  # attr_reader :name, :address, :phone, :services
-  attr_accessor :name, :address, :phone, :services
+  attr_accessor :name, :address, :phone, :services, :registered_vehicles, :collected_fess
 
   def initialize(info)
     @name = info[:name]
     @address = info[:address]
     @phone = info[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fess = 0
   end
 
   def add_service(service)
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -11,6 +14,8 @@ RSpec.describe Facility do
       expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
       expect(@facility.phone).to eq('(720) 865-4600')
       expect(@facility.services).to eq([])
+      expect(@facility.registered_vehicles).to eq([])
+      expect(@facility.collected_fess).to eq(0)
     end
   end
 
