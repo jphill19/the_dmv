f538edce7edec55988014c34a671e67a10054deb
John Hill <jphill1989@gmail.com>
2024-07-11 15:19:12 -0600

    Created the class alongside an init with 3 parameters

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,3 +1,12 @@
+#type error link: https://www.exceptionalcreatures.com/bestiary/TypeError.html
+
 class Registrant
 
+  def initialize(name, age, license_data = false)
+    raise TypeError, "Name must be a String" unless name.is_a?(String)
+    raise TypeError, "Age must be an Integer" unless age.is_a?(Integer)
+
+    @name = name
+    @age = age
+    @license_data = license_data
 end
