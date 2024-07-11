5a7507c97422b0b9231a075c9d4253645a724b87
John Hill <jphill1989@gmail.com>
2024-07-11 15:42:57 -0600

    Added two test for the initialize function for my registrant object

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -5,8 +5,11 @@ class Registrant
   def initialize(name, age, license_data = false)
     raise TypeError, "Name must be a String" unless name.is_a?(String)
     raise TypeError, "Age must be an Integer" unless age.is_a?(Integer)
+    raise TypeError, "License_data must be a Boolean" unless [TrueClass, FalseClass].include?(license_data.class)
 
     @name = name
     @age = age
     @license_data = license_data
+  end
+
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -9,6 +9,30 @@ RSpec.describe Registrant do
     @registrant_2 = Registrant.new('Penny', 15 )
   end
 
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_an_instance_of(Registrant)
+    end
+
+    it 'accepts proper values only' do
+      # registrant_bad_name = Registrant.new(13, 15, false )
+      # registrant_bad_age = Registrant.new("Timmy", 13.1)
+      # registrant_bad_data = Registrant.new("Timmy", 15, "yes")
+      #bad name test
+      expect {Registrant.new(13, 15, false ) }.to raise_error(TypeError, "Name must be a String")
+
+      #bad age test
+      expect {Registrant.new("Timmy", 13.1) }.to raise_error(TypeError, "Age must be an Integer")
+
+      #bad license_data test
+      expect {Registrant.new("Timmy", 15, "yes")}.to raise_error(TypeError, "License_data must be a Boolean")
+
+  end
+
+end
+
+ 
+
 
 
 
