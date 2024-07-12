570ceff70c27b27ce68f629886392f6d5530b3a4
John Hill <jphill1989@gmail.com>
2024-07-12 17:15:17 -0600

    Created the create_vehicles method inside vehicle_factory, and am testing the output

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -6,7 +6,15 @@ class VehicleFactory
 
   def create_vehicles(data)
     data.each do |vehicle|
-      new_vehicle = Vehicle.new({vin: vehicle[:vin_1_10],})
+      new_vehicle = Vehicle.new({
+        vin: vehicle[:vin_1_10],
+        year: vehicle[:model_year],
+        make: vehicle[:make],
+        model: vehicle[:model],
+        engine: :ev
+      })
+
+      @vehicles_created << new_vehicle
     end
 
   end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 RSpec.describe Vehicle do
   before(:each) do
     @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
 
   describe "#initialize" do
     it "can initialize" do
       expect(@factory).to be_an_instance_of(VehicleFactory)
     end
-
   end
 
+  describe "#create_vehicles" do
+  
 
 end
