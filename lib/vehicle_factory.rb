15f68e95a2610558ecce9c869dcb6b45e4b6ff11
John Hill <jphill1989@gmail.com>
2024-07-12 17:11:42 -0600

    Created a sandbox file, and found a way to tap into the data & iterate over it

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,4 +4,11 @@ class VehicleFactory
     @vehicles_created = []
   end
 
+  def create_vehicles(data)
+    data.each do |vehicle|
+      new_vehicle = Vehicle.new({vin: vehicle[:vin_1_10],})
+    end
+
+  end
+
 end
---
+++ spec/sandbox.rb
@@ -0,0 +1,9 @@
+require "./lib/dmv_data_service"
+
+wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+wa_ev_registrations.each do |vehicle|
+
+  puts vehicle[:vin_1_10]
+
+end
