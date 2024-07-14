e5e74b0a092c1230c207cfd1ae781bba7361d4ff
John Hill <jphill1989@gmail.com>
2024-07-14 08:45:01 -0600

    Found all the ways to tap into DmvDataService.new.co_dmv_office_locations dataset for our faciliity within sandbox

--- spec/sandbox.rb
+++ spec/sandbox.rb
@@ -1,7 +1,7 @@
 require "./lib/dmv_data_service"
 
 
-wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+# wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
 # wa_ev_registrations.each do |vehicle|
 
@@ -9,4 +9,15 @@ wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
 # end
 
-puts wa_ev_registrations
+
+new_data = DmvDataService.new.co_dmv_office_locations
+
+
+# puts new_data[0]
+
+puts new_data[0][:dmv_office]
+
+puts new_data[0][:address_li] + ' ' +  new_data[0][:address__1] + ' ' +  new_data[0][:city] + ' ' +  new_data[0][:state] + ' ' + new_data[0][:zip] 
+
+puts new_data[0][:phone]
+
